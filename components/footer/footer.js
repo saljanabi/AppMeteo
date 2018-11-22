57061d53bf49f1769044b949314304d32c91a912
Sarah <sarah.aljanabi@gmail.com>
2018-11-22 23:35:43 +0100

    Adds json parser to app.js

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,35 +3,35 @@ import { Footer, FooterTab, Button } from 'native-base';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { DocumentPicker, FileSystem } from 'expo';
 import StyleSheet from '../../src/style/styles';
+import { checkPropTypes } from 'prop-types';
 
 export default class FooterTabs extends Component {
   state = {
     data: null,
   };
-  // Pick document
+  // Initialisation de la méthode pickDocument qui permet de sélectionner un fichier dans le répertoire Download de l'appareil
   _pickDocument = async () => {
     let result = await DocumentPicker.getDocumentAsync({
         type: '*/*',
     });
-
+    // Si sélection valide alors on quitte le dossier en cours pour afficher l'onglet principal de l'app
     if (result.type === 'cancel') {
       console.log('cancel');
       return;
     }
-
-    const info = await FileSystem.getInfoAsync(result.uri);
-    this.state = { data: info };
+    // Récupération du contenu du fichier qui sera utilisé dans le parser json ensuite. 
     const content = await FileSystem.readAsStringAsync(result.uri);
     
+    // Chaque (...) du fichier sera séparé par un saut de ligne
     let lines = content.split('\n');
-
+    //On initialise notre tableau avec res
     let res = [];
     let i = 1;
     let j = 0;
     while (lines[i]) {
       columns = lines[i].split('\t');
       if (i == 1) {
-        //1ere itération de la boucle : on récupere le titre des colonnes
+        //Première itération de la boucle : on récupere le titre des colonnes
         titles = columns;
       } else {
         res[i - 2] = {};
@@ -44,11 +44,29 @@ export default class FooterTabs extends Component {
       }
       i++;
     }
-    let filename = res[2].CREATEDATE;
+    let dir = FileSystem.cacheDirectory + 'meteo-etna';
+    let filename = res[0].CREATEDATE + '.json';
+    let json = JSON.stringify(res);
 
-    console.log('res 2 = ', res[2]);
-    console.log('filename', filename);
-    this.state = { data: res };
+    const dir_info = await Expo.FileSystem.getInfoAsync(dir);
+    if (!Boolean(dir_info.exists)) {
+      try {
+        await FileSystem.makeDirectoryAsync(dir, {
+          intermediates: true
+        });
+      } catch(e) {
+        console.log("could not create directory " + dir);
+        console.log(e);
+      }
+    }
+    console.log(dir);
+    console.log(FileSystem.cacheDirectory);
+
+    FileSystem.writeAsStringAsync(dir + filename, json);
+    this.state = { 
+      data: res,
+      filename : filename
+    };
   }
   render() {
     return (
